2561fe13d72671467f142843cbee8e113b33925b
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-07-23 11:39:16 -0700

    added routes

--- server.js
+++ server.js
@@ -5,6 +5,24 @@ const path = require('path');
 const app = express();
 const PORT = process.env.port || 3001;
 
+// routes
+app.get('/notes', (req, res) => {
+
+});
+
+app.get('/api/notes', (req, res) => {
+
+});
+
+app.post('/api/notes', (req, res) => {
+
+});
+
+app.get('*', (req, res) => {
+
+});
+
+// Listener
 app.listen(PORT, () => {
     console.log(`App listening on PORT http://localhost:${PORT}`);
 });
