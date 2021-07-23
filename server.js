c392079f529255f1493401513b5490a8ee374373
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-07-23 15:40:35 -0700

    DELETE path deletes the appropiate note when delete button is pressed.

--- server.js
+++ server.js
@@ -16,13 +16,13 @@ app.use(express.static('public'));
 
 // routes
 app.get('/notes', (req, res) => {
-    console.log(`GET Path: ${req.url}`);
+
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
 // return the current notes by reading `db.json`
 app.get('/api/notes', (req, res) => {
-    console.log(`GET Path: ${req.url}`);
+
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {
             console.err(err);
@@ -33,8 +33,8 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-    console.log(`POST Path: ${req.url}`);
-    const {title, text} = req.body; // destructure note object into its properties
+
+    const { title, text } = req.body; // destructure note object into its properties
 
     if (req.body) {
         const newNote = {
@@ -67,22 +67,46 @@ app.post('/api/notes', (req, res) => {
 
 });
 
-// get current notes by reading `db.json`
-// find the note using the provided id
-// delete the note from the collection
-// rewrite collection to `db.json`
+
 app.delete('/api/notes/:id', (req, res) => {
     console.log(`DELETE Path: ${req.url}`);
-    console.log(`id: ${req.body.id}`);
-    res.send(`${req.body.id}`);
+
+    if (req.params.id) {
+        const noteId = req.params.id;
+
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.err(err);
+            } else {
+                const noteCollection = JSON.parse(data); // put db.json into array of objects
+
+                for (let i = 0; i < noteCollection.length; i++) {
+                    if (noteCollection[i].id === noteId) {
+                        noteCollection.splice([i], 1);
+                        break; // stop iterating because each id is unique
+                    };
+                };
+
+                // overwrite new collection into db.json
+                fs.writeFile("./db/db.json", JSON.stringify(noteCollection, null, 4), (err) => {
+                    if (err) throw err;
+                    console.log('Saved db.json');
+                });
+            };
+        });
+        
+        res.send(`Deleted`);
+    } else {
+        res.error('Error deleting note')
+    };
+
 });
 
+// wildcard path
 app.get('*', (req, res) => {
     console.log(`GET Wildcard: ${req.url}`);
     res.sendFile(path.join(__dirname, '/public/index.html'))
 });
-
-
 
 // Listener
 app.listen(PORT, () => {
