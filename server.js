ced4b2c0555116a706a7ab1c4263cbda3cce100c
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-07-23 14:03:43 -0700

    post route can take response body data and add it to db.json

--- server.js
+++ server.js
@@ -19,19 +19,59 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
-// get the currents notes by reading `db.json`
+// return the currents notes by reading `db.json`
 app.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {
             console.err(err);
         } else {
-            res.json(JSON.parse(data))
+            res.json(data)
         }
     });
 });
 
+// get current notes by reading `db.json`
+// append a new note to the collection
+// save the new collection of notes to `db.json`
 app.post('/api/notes', (req, res) => {
-    res.send("Posted, man")
+    const {title, text} = req.body; // destructure note object into its properties
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuidv4()
+        };
+
+        // readAndAppend function(s) here
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.err(err);
+            } else {
+                const noteCollection = JSON.parse(data); // put db.json into variable
+                console.log(`Old noteCollection === ${JSON.stringify(noteCollection)}`);
+                noteCollection.push(newNote); // add new note to the array
+
+                fs.writeFile("./db/db.json", JSON.stringify(noteCollection), (err) => {
+                    if (err) throw err;
+                    console.log('Saved db.json');
+                });
+
+                res.send(`new noteCollection === ${JSON.stringify(noteCollection)}`);
+            }
+        });
+
+    };
+
+
+});
+
+// get current notes by reading `db.json`
+// find the note using the provided id
+// delete the note from the collection
+// rewrite collection to `db.json`
+app.delete('/api/notes/:id', (req, res) => {
+    res.send();
 });
 
 app.get('*', (req, res) => {
