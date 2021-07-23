b7e2e2d0e7046ae16ca12ab900e236058e0fa9ac
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-07-23 11:51:42 -0700

    added middleware

--- server.js
+++ server.js
@@ -5,21 +5,26 @@ const path = require('path');
 const app = express();
 const PORT = process.env.port || 3001;
 
+// middleware
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
 // routes
 app.get('/notes', (req, res) => {
-
+    res.send(req.url)
 });
 
 app.get('/api/notes', (req, res) => {
-
+    res.send(req.url)
 });
 
 app.post('/api/notes', (req, res) => {
-
+    res.send("Posted, man")
 });
 
 app.get('*', (req, res) => {
-
+    res.send("Catch All")
 });
 
 // Listener
