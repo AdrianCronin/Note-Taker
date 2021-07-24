b00aec5681ea9f06c6d470660a6718e93c8231b9
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-07-23 17:33:01 -0700

    fixed process.env.PORT

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { v4: uuidv4 } = require('uuid'); // uuidv4(); ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
 
 const app = express();
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 // middleware
 app.use(express.urlencoded({ extended: true }));
